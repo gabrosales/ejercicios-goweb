0d1a070f5725910f990edcfca0f351eeee224edd
gabrosales <gabrielfrancisco.rosales@mercadolibre.com>
2023-01-05 13:36:11 -0300

    Make handlers outside main

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -27,15 +26,63 @@ var products []Product
 
 // Funcion de carga de datos
 func LoadProducts() {
-	jsonFile, err := os.Open("products.json")
+	jsonFile, err := os.ReadFile("products.json")
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer jsonFile.Close()
+	// defer jsonFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
+	// byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	json.Unmarshal(byteValue, &products)
+	json.Unmarshal(jsonFile, &products)
+
+}
+
+func Ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func GetAllProducts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, products)
+}
+
+func GetProductById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Fail to parse id."})
+		return
+	}
+
+	for _, pro := range products {
+		if pro.ID == id {
+			c.IndentedJSON(http.StatusOK, pro)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Id not found"})
+}
+
+func SearchProductsByPrice(c *gin.Context) {
+	priceGt, err := strconv.Atoi(c.Query("priceGt"))
+
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Fail to parse price."})
+		return
+	}
+
+	var productsFiltered []Product
+
+	for _, pro := range products {
+		if pro.Price > float64(priceGt) {
+			productsFiltered = append(productsFiltered, pro)
+		}
+	}
+
+	if productsFiltered != nil {
+		c.IndentedJSON(http.StatusOK, productsFiltered)
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "There is no products with that price."})
 
 }
 
@@ -46,54 +93,14 @@ func main() {
 	r := gin.Default()
 
 	//PING
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+	r.GET("/ping", Ping)
 	//Produtcs
-	r.GET("/products", func(c *gin.Context) {
-		c.IndentedJSON(http.StatusOK, products)
-	})
+	r.GET("/products", GetAllProducts)
 	//Product:id
-	r.GET("/products/:id", func(c *gin.Context) {
-		id, err := strconv.Atoi(c.Param("id"))
-
-		if err != nil {
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Fail to parse id."})
-			return
-		}
-
-		for _, pro := range products {
-			if pro.ID == id {
-				c.IndentedJSON(http.StatusOK, pro)
-				return
-			}
-		}
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Id not found"})
-	})
+	r.GET("/products/:id", GetProductById)
 
 	//Product search x price
-	r.GET("/products/search", func(c *gin.Context) {
-		priceGt, err := strconv.Atoi(c.Query("priceGt"))
-
-		if err != nil {
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Fail to parse price."})
-			return
-		}
-
-		var productsFiltered []Product
-
-		for _, pro := range products {
-			if pro.Price > float64(priceGt) {
-				productsFiltered = append(productsFiltered, pro)
-			}
-		}
-
-		if productsFiltered != nil {
-			c.IndentedJSON(http.StatusOK, productsFiltered)
-		}
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "There is no products with that price."})
-
-	})
+	r.GET("/products/search", SearchProductsByPrice)
 
 	r.Run() // escucha en 0.0.0.0:8080 por defecto
 
